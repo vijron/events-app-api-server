1e22e2efa7350025af7b557af2634ed0663bd5b5
vijron <promodeals76@gmail.com>
2022-10-06 16:54:46 +0000

    Team name change

--- src/app.config.js
+++ src/app.config.js
@@ -4,7 +4,7 @@
  * DO NOT store state here - must be part of a stateless service
  */
 class AppConfig {
-    team = 'Team 3';
+    team = 'VB';
     version = '1.0.1';
 }
 module.exports = AppConfig;
